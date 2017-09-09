4ccd40c0a1ff115f7d31c7bdbab0e04f9be37b7c
vimrus <vimrus.com@gmail.com>
2017-09-09 18:49:17 +0800

    * fix.

---
+++ index.js
@@ -0,0 +1,30 @@
+hubble.getXML('https://www.huxiu.com/rss/0.xml', function (error, response, $) {
+	$('item').each(function (index, value) {
+
+		var url = $(this).find('link').text();
+		var key = url.substring(url.lastIndexOf('/') + 1, url.lastIndexOf('.html'));
+		var dom = $(this);
+
+		articles.get('key', key, function (article) {
+			if (article) {
+				return;
+			}
+
+			var title   = dom.find('title').text().trim();
+			var summary = dom.find('description').text().replace(/<\/?[^>]*>/g,'').trim().substring(0, 50);
+			var content = dom.find('content').text();
+			var $ = cheerio.load(content);
+			var image = $('img').eq(0).attr('src');
+
+			var article = {
+				key: key,
+				title: title,
+				content: content,
+				summary: summary,
+				url: url,
+				image: image
+			};
+			articles.append(article);
+		});
+	});
+});
